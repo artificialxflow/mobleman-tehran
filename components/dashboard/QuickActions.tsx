9176ffd8556a4c3c9afff376b576983d5551c987
zedparman <ze.dparman@gmail.com>
2025-09-06 05:23:18 -0700

    Update QuickActions.tsx

--- components/dashboard/QuickActions.tsx
+++ components/dashboard/QuickActions.tsx
@@ -37,12 +37,12 @@ export default function QuickActions() {
       <h5 className="mb-3">اقدامات سریع</h5>
       <div className="row g-2">
         {quickActions.map((action, index) => (
-          <div key={index} className="col-6">
+          <div key={index} className="col-12 col-sm-6">
             <div className={`card h-100 border-0 bg-${action.color} bg-opacity-10 hover-lift`}>
-              <div className="card-body text-center p-3">
-                <i className={`${action.icon} text-${action.color} fs-2 mb-2`}></i>
+              <div className="card-body text-center p-2 p-md-3">
+                <i className={`${action.icon} text-${action.color} fs-3 fs-md-2 mb-2`}></i>
                 <h6 className="card-title small mb-1">{action.title}</h6>
-                <p className="card-text" style={{ fontSize: '0.75rem' }}>{action.description}</p>
+                <p className="card-text d-none d-sm-block" style={{ fontSize: '0.75rem' }}>{action.description}</p>
               </div>
             </div>
           </div>
