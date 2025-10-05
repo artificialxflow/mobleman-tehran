9aab8f655284da6df0d3b1cf9a5cb514b92f38b3
zedparman <ze.dparman@gmail.com>
2025-10-05 09:03:42 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { CompanyProvider } from "../mobleman-tehran/hooks/use-company";
+import { PermissionsProvider } from "../mobleman-tehran/hooks/use-permissions";
+import Layout from "../components/Layout";
 
 export const metadata: Metadata = {
   title: "سیستم ERP مبلمان تهران",
@@ -19,7 +22,13 @@ export default function RootLayout({
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
       </head>
       <body>
-        {children}
+        <CompanyProvider>
+          <PermissionsProvider>
+            <Layout>
+              {children}
+            </Layout>
+          </PermissionsProvider>
+        </CompanyProvider>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </html>
