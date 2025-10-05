700f39390a67de1d4a2c08991adde6682978abec
zedparman <ze.dparman@gmail.com>
2025-10-05 10:40:52 -0700

    Update page.tsx

--- app/workflows/automated-reporting/page.tsx
+++ app/workflows/automated-reporting/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import React from 'react';
-import Layout from '@/components/Layout';
 
 export default function AutomatedReportingWorkflowPage() {
   return (
-    <Layout>
+      <>
       <div className="container-fluid">
         <div className="d-flex justify-content-between align-items-center mb-4">
           <div>
@@ -457,6 +456,6 @@ export default function AutomatedReportingWorkflowPage() {
           border-bottom: none;
         }
       `}</style>
-    </Layout>
+      </>
   );
 }
