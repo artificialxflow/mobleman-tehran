72fb0f64458b74fd9707f4e909cc980e40be36da
zedparman <ze.dparman@gmail.com>
2025-10-05 23:36:22 -0700

    Update page.tsx

--- app/orders/processing/page.tsx
+++ app/orders/processing/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../mobleman-tehran/app/orders/processing/page.tsx';
+export { default } from '../../../mobleman-tehran/app/orders/processing/page';
 
 
