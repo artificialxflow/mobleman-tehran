d79fa034b75b19596da80c502bbb8b173414c87f
zedparman <ze.dparman@gmail.com>
2025-10-05 23:20:43 -0700

    Update page.tsx

--- app/products/customization/page.tsx
+++ app/products/customization/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../mobleman-tehran/app/products/customization/page';
+export { default } from '../../../mobleman-tehran/app/products/customization/page.tsx';
 
 
