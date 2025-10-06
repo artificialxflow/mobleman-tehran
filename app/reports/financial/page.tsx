67deb65288f08ae4fcac88929f96513daee374ea
zedparman <ze.dparman@gmail.com>
2025-10-05 23:20:54 -0700

    Update page.tsx

--- app/reports/financial/page.tsx
+++ app/reports/financial/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../mobleman-tehran/app/reports/financial/page';
+export { default } from '../../../mobleman-tehran/app/reports/financial/page.tsx';
 
 
