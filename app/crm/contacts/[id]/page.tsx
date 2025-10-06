559d2b370f01b45db0c7bdaff13423ed3c5128ce
zedparman <ze.dparman@gmail.com>
2025-10-05 23:22:34 -0700

    Update page.tsx

--- app/crm/contacts/[id]/page.tsx
+++ app/crm/contacts/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../../mobleman-tehran/app/crm/contacts/[id]/page';
+export { default } from '../../../../mobleman-tehran/app/crm/contacts/[id]/page.tsx';
 
 
