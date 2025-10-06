301bb504f4b8099d9db417a969fe164951648a77
zedparman <ze.dparman@gmail.com>
2025-10-05 23:36:09 -0700

    Update page.tsx

--- app/warehouse/inventory/page.tsx
+++ app/warehouse/inventory/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../mobleman-tehran/app/warehouse/inventory/page.tsx';
+export { default } from '../../../mobleman-tehran/app/warehouse/inventory/page';
 
 
