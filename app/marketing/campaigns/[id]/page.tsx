7a3192174728cba84fe533e23822187eb4842584
zedparman <ze.dparman@gmail.com>
2025-10-05 23:21:51 -0700

    Update page.tsx

--- app/marketing/campaigns/[id]/page.tsx
+++ app/marketing/campaigns/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../../mobleman-tehran/app/marketing/campaigns/[id]/page';
+export { default } from '../../../../mobleman-tehran/app/marketing/campaigns/[id]/page.tsx';
 
 
