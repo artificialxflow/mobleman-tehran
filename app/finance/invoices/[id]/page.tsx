e9d4d8d872a8f0dc30f5c51c337ce03d15de6f1c
zedparman <ze.dparman@gmail.com>
2025-10-05 23:37:01 -0700

    Update page.tsx

--- app/finance/invoices/[id]/page.tsx
+++ app/finance/invoices/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
-export { default } from '../../../../mobleman-tehran/app/finance/invoices/[id]/page.tsx';
+export { default } from '../../../../mobleman-tehran/app/finance/invoices/[id]/page';
 
 
